cbc2d822194e19780bd5c1b21697e09e373f73b5
erikk <ekeszler77@gmail.com>
2023-08-29 17:32:56 +0300

    problema 8 sesiunea 15 *functioneaza 8.10

--- src/OOP/Sesiunea15/ex8_companyapp/Company.java
+++ src/OOP/Sesiunea15/ex8_companyapp/Company.java
@@ -1,5 +1,7 @@
 package OOP.Sesiunea15.ex8_companyapp;
 
+import java.util.Arrays;
+
 public class Company {
 
    private String name;
@@ -9,6 +11,53 @@ public class Company {
    private Department[] departments;
 
    private int numberOfDepartmentsAdded;
+
+   public Company(String name, int yearFounded, Department[] departments, int numberOfDepartmentsAdded) {
+      this.name = name;
+      this.yearFounded = yearFounded;
+      this.departments = departments;
+      this.numberOfDepartmentsAdded = numberOfDepartmentsAdded;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public int getYearFounded() {
+      return yearFounded;
+   }
+
+   public void setYearFounded(int yearFounded) {
+      this.yearFounded = yearFounded;
+   }
+
+   public Department[] getDepartments() {
+      return departments;
+   }
+
+   public void setDepartments(Department[] departments) {
+      this.departments = departments;
+   }
+
+   public int getNumberOfDepartmentsAdded() {
+      return numberOfDepartmentsAdded;
+   }
+
+   public void setNumberOfDepartmentsAdded(int numberOfDepartmentsAdded) {
+      this.numberOfDepartmentsAdded = numberOfDepartmentsAdded;
+   }
+
+   public void printAllDepartmentsOfCompany(){
+      //parcurgem array-ul si imprimam fiecare departament intr-un "for"
+      for (int i = 0; i < numberOfDepartmentsAdded; i++) {
+         System.out.println(departments[i].getName());
+      }
+   }
+
 
    public void printAllEmployeesStreetsByDepartment(String departmentName){
        //cautam departamentul in lista de departamente a companiei pe care se apeleaza metoda
--- src/OOP/Sesiunea15/ex8_companyapp/Main.java
+++ src/OOP/Sesiunea15/ex8_companyapp/Main.java
@@ -4,18 +4,25 @@ public class Main {
     public static void main(String[] args) {
         Adress adress1 = new Adress("Los Angeles", "rich street", 1);
         Adress adress2 = new Adress("Los Angeles", "rich street", 2);
+        Adress adress3 = new Adress("Los Angeles", "rich street", 3);
         Employee employee1 = new Employee("Ionica", "Georgel", 10000, adress1);
         Employee employee2 = new Employee("Ionica", "Marcel", 10000, adress2);
+        Employee employee3 = new Employee("Ionica", "Mirel", 9000, adress3);
         Employee[] firstDepEmpList = {employee1, employee2};
+        Employee[] secondDepEmpList = {employee3};
 //        System.out.println(employee1.getName());
 //        System.out.println(employee1.getAdressStreet());
-        Department department = new Department("dep1", firstDepEmpList, 2);
-        department.printAllEmployeesNames();
-        department.printAllEmployeesAdressStreets();
-        department.printAllEmployeesAdress();
-        department.printNumberOfEmployeeAdded();
-        department.searchEmployeeByName();
-        department.printSpecificEmployeesAdress();
-        department.searchAdressByStreet();
+        Department department1 = new Department("dep1", firstDepEmpList, 2);
+        Department department2 = new Department("dep2", secondDepEmpList, 1);
+        department1.printAllEmployeesNames();
+        department1.printAllEmployeesAdressStreets();
+        department1.printAllEmployeesAdress();
+        department1.printNumberOfEmployeeAdded();
+        department1.searchEmployeeByName();
+        department1.printSpecificEmployeesAdress();
+        department1.searchAdressByStreet();
+        Department[] departments = {department1, department2};
+        Company company = new Company("comp1",2002,departments, 2);
+        company.printAllDepartmentsOfCompany();
     }
 }
